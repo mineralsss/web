f66590be2733dde86200d1b83695b23b62bbb752
mineralsss <tuananh171312@outlook.com>
2021-11-05 15:46:12 +0700

    commit

--- web-frontend/pages/index.js
+++
@@ -1,5 +0,0 @@
-function HomePage() {
-  return <div>This is Minerals' stupid page</div>
-}
-
-export default HomePage
---
+++ web-frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hi this is mineral's stupid page</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
